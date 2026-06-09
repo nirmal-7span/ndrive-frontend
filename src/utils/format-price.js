a34ac2ad373cab6f8065017b35a58430de835a2c
nirmal-7span <nirmal@7span.com>
2026-06-09 19:06:09 +0530

    feat: add cars json api and utils functions

---
+++ src/utils/format-price.js
@@ -0,0 +1,9 @@
+function formatPrice(price) {
+    return new Intl.NumberFormat('en-IN', {
+        style: 'currency',
+        currency: 'INR',
+        maximumFractionDigits: 0,
+    }).format(price);
+}
+
+export default formatPrice;
