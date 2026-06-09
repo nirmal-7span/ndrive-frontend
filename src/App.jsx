1a739cf20d61e90ecd56e587545640b942cff359
nirmal-7span <nirmal@7span.com>
2026-06-09 16:34:11 +0530

    chore: setup done and added ui components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import { Button } from "./components/ui/button";
+
 function App() {
   return (
     <div className="min-h-screen flex items-center justify-center">
-      <h1 className="text-5xl font-bold bg-red-200">NDrive</h1>
+      <Button>NDrive</Button>
+      <Button variant="outline">Outline</Button>
+      <Button variant="destructive">Destructive</Button>
+      <Button variant="ghost">Ghost</Button>
+      <Button variant="link">Link</Button>
     </div>
   );
 }
